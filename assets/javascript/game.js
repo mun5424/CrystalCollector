4f3237e06f8c7f09d157cb4e0e57748460e8192d
Charlie Oh <mun5424@gmail.com>
2018-02-12 14:33:14 -0800

    jquery updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,13 +2,31 @@ var startgame = 0;
 
 
 $(document).ready(function(){
- 
-    $(".crystal").on("click", function() {
-        
-        var crystal = $("<div>");
-        crystal.
-        $("#display").append(fridgeMagnet);
+    var wins = 0;
+    var losses = 0;
+    var compScore = 0; 
+    var myScore = 0; 
 
+
+    startGame = function() {
+        $(".crystal").each(function() {
+            var randCrytal = Math.floor((Math.random() * 12) + 1);
+               $(this).attr("value", randCrytal);
+         });
+         compScore = Math.floor((Math.random() * 102) + 19);
+         
+         $("#computer-score").text(compScore); 
+         $("#my-score").text(0); 
+    
+    }
+
+    $(".crystal").on("click", function() {
+        var crystal = $(this);
+        alert(crystal.attr("value"));
+        
       });
+
+      
+    startGame();
 
 });
