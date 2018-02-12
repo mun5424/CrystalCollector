c9687cca3e45c9a773d71f8bd914b75bfa2d6bd7
Charles Oh <mun5424@gmail.com>
2018-02-12 00:46:54 -0800

    First commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+var startgame = 0; 
+
+
+$(document).ready(function(){
+ 
+    $(".crystal").on("click", function() {
+        
+        var crystal = $("<div>");
+        crystal.
+        $("#display").append(fridgeMagnet);
+
+      });
+
+});
