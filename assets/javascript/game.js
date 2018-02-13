af7cce3678ebc6b143a7c4c2ecf629f9fc1fe8f3
Charles Oh <mun5424@gmail.com>
2018-02-12 22:46:04 -0800

    made some changes to css for mobile. Finished making all javascript & jquery calls

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,20 +9,48 @@ $(document).ready(function(){
 
 
     startGame = function() {
-        $(".crystal").each(function() {
-            var randCrytal = Math.floor((Math.random() * 12) + 1);
-               $(this).attr("value", randCrytal);
-         });
-         compScore = Math.floor((Math.random() * 102) + 19);
          
-         $("#computer-score").text(compScore); 
-         $("#my-score").text(0); 
-    
+         resetScores(); 
     }
 
+    resetScores = function() {
+        //reset scores. mine to 0 and computer to random
+        myScore = 0; 
+        $("#my-score").text(0); 
+        compScore = getRndInteger(20,120) ;
+        $("#computer-score").text(compScore); 
+
+        //give each crystal a random value between 1-12
+        $(".crystal").each(function() {
+            var randCrytal = getRndInteger(1,12);
+               $(this).attr("value", randCrytal);
+         });
+    }
+
+    function getRndInteger(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) ) + min;
+    }
+
+
     $(".crystal").on("click", function() {
+        //clear status msg
+        $("#status").text(" ");
+
         var crystal = $(this);
-        alert(crystal.attr("value"));
+        myScore += parseInt(crystal.attr("value"));
+        $("#my-score").text(myScore); 
+        if (myScore === compScore)
+        {
+            resetScores(); 
+            $("#wins").text("WINS: " + ++wins); 
+            $("#status").text("YOU WIN! ");
+        }
+        if (myScore > compScore)
+        {
+            resetScores(); 
+            $("#losses").text("LOSSES: " + ++losses); 
+            $("#status").text("YOU LOSE! ");
+        }
         
       });
 
